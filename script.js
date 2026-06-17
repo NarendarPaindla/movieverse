45dd54887942041e5b3f112c0db5c28586fecc44
NarendarPaindla <narendarreddypaindla@gmail.com>
2026-06-17 12:59:32 +0530

    phase8:-->loading ... option

--- script.js
+++ script.js
@@ -3,11 +3,15 @@ const API_KEY="26b9ad21";
 const searchBtn=document.getElementById("searchBtn");
 const searchInput=document.getElementById("searchInput");
 const moviesContainer=document.getElementById("moviesContainer");
+const loader=document.getElementById("loader");
+
 
 async function searchMovies(movieName){
+    loader.style.display = "flex";
     const response=await fetch(`https://www.omdbapi.com/?apikey=${API_KEY}&s=${movieName}`);
     console.log(response);
     const data=await response.json();
+    loader.style.display="none";
    if (data.Response === "False"){
     moviesContainer.innerHTML="<h2>No Movies Found</h2>";
     return;
